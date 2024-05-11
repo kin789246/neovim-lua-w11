bb0df82713124cdafb41b243fa16c692a49cb9cf
j-ching69 <ocezask@hotmail.com>
2024-05-11 19:33:18 +0800

    first commit

---
+++ after/plugin/vscode-theme.lua
@@ -0,0 +1,9 @@
+require('vscode').setup({
+  transparent = true,
+  italic_comments = true,
+  color_overrides = {
+        vscCursorDark = '#63f2ff',
+        vscTabOutside = '#9e9e9e'
+  },
+})
+require('vscode').load()
